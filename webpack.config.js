cfb8ac25f83b520b9e7ec7388ae3d31f36a068b3
Adarsh <21104039.adarsh.pandey@gmail.com>
2023-09-30 22:49:45 +0530

    final

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  // ...other Webpack configuration settings...
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+};
